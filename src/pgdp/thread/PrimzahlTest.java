2c337f801d968befda7170f0375d45a5509d5857
martin <ga27kin@mytum.de>
2021-02-01 21:37:33 +0100

    add comment

--- src/pgdp/thread/PrimzahlTest.java
+++ src/pgdp/thread/PrimzahlTest.java
@@ -3,6 +3,9 @@ package pgdp.thread;
 import java.util.function.IntFunction;
 
 public class PrimzahlTest {
+	// This indirect call of the function is required to test your tests in
+	// artemis. With the function being an attribute it can be manipulated to
+	// insert a faulty function and check whether your tests detect it.
 	public static IntFunction<Boolean> testFunc;
 
 	public static boolean isPrime(int x) {
